6af4081288a0e9cffc1b8736dc72a079a0728d3d
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-14 23:57:33 +0000

    tests not running. rake aborted

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -1,6 +1,20 @@
 # This method takes an array of elements (these might be strings, integers, floats, or a
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
-def find_unique_elements(arr)
+
+def count_elements(arr)
+  h={}
+  arr.each do |n|
+    if h.has_key?(n) == false
+      h[n]=1
+    elsif h.has_key?(n) == true
+      h[n]= h[n] +1
+    end
+  end
   
+  
+def find_unique_elements(arr)
+    a = count_elements(arr)
+    b = a.hash.index(1)
+    returns b
 end
